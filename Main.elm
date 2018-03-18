6ada22fbcf79eb30a27a19c18ad40e7321d8f619
Deepak Duggirala <deepak.duggirala@target.com>
2018-03-18 19:40:35 +0530

    draw block using svg

--- Main.elm
+++ Main.elm
@@ -3,12 +3,20 @@ import Keyboard
 import Char
 import AnimationFrame
 import Time exposing (Time)
+import Svg exposing (svg, rect, g)
+import Svg.Attributes exposing (..)
 
 -- MODEL
-type alias Model = String
+type alias Position =
+  {
+    x: Int,
+    y: Int
+  }
+
+type alias Model = Position
 
 init : ( Model, Cmd Msg )
-init = ( "", Cmd.none )
+init = ( Position 100 100, Cmd.none )
 
 type Msg =
   KeyCode Int
@@ -33,28 +41,48 @@ toDirection k =
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    Tick time -> (toString time, Cmd.none)
+    Tick time -> (model, Cmd.none)
     KeyCode k -> let
       dir = toDirection k
     in
       case dir of
-        Nothing -> ("Other", Cmd.none)
-        Just d -> (toString d, Cmd.none)
-    
+        Nothing -> (model, Cmd.none)
+        Just d -> (updatePosition d model, Cmd.none)
+
+updatePosition : Direction -> Position -> Position
+updatePosition d pos =
+  case d of
+    Up -> Position pos.x (pos.y + 1)
+    Right -> Position (pos.x + 1) pos.y
+    Down -> Position pos.x (pos.y - 1)
+    Left -> Position (pos.x - 1) pos.y
 
 view : Model -> Html.Html Msg
 view model =
   div []
     [
-      h2 [] [text "Direction"], 
-      text model
+      div [style "float: left"]
+        [
+          svg [ version "1.1", baseProfile "full", width "600",  height "600", viewBox "0 0 1000 1000", style "border: black 4px solid"]
+            [
+              g [ transform "matrix(1,0,0,-1,0,1000)" ]
+                [
+                  block model
+                ]
+            ]
+        ],
+      div [style "float: right"] [text <| toString model]
     ]
+
+block : Position -> Html.Html Msg
+block pos =
+  rect [ x <| toString pos.x, y <| toString pos.y, width "10", height "10", fill "black", stroke "black"] []
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.batch [
-      Keyboard.downs KeyCode,
-      AnimationFrame.times Tick
+      Keyboard.downs KeyCode
+      -- AnimationFrame.times Tick
     ]
 
 
