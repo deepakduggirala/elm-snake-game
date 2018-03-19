aa45f93a8f2da0dcba3bf3a164b4209d287a712f
Deepak Duggirala <deepak.duggirala@target.com>
2018-03-19 22:12:20 +0530

    make grid responsive

--- Main.elm
+++ Main.elm
@@ -5,6 +5,9 @@ import Time exposing (Time)
 import Svg exposing (svg, rect, g)
 import Svg.Attributes exposing (..)
 import Html.Events exposing (onClick)
+import Window
+import Task exposing (Task)
+
 
 -- MODEL
 type alias Position =
@@ -13,19 +16,27 @@ type alias Position =
     y: Int
   }
 
+type alias Grid =
+  {
+    width: Int,
+    height: Int
+  }
+
 type alias Model = {
   attitude: Direction,
   body: List Position,
-  dead: Bool
+  dead: Bool,
+  grid: Grid
 }
 
 init : ( Model, Cmd Msg )
-init = ( Model Up (List.map (\i -> (Position (50-i) 50)) (List.range 0 29) ) False, Cmd.none )
+init =  ( Model Up (List.map (\i -> (Position (50-i) 50)) (List.range 0 29) ) False (Grid 0 0), Task.perform (\{width, height} -> Resize height width) Window.size )
 
 type Msg =
   KeyCode Int
   | Tick Time
   | Restart
+  | Resize Int Int
 
 type Direction =
   Up
@@ -56,16 +67,17 @@ isPerpendicular d1 d2 =
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    Tick time -> (Model model.attitude (updateBody model.attitude model.body) (isDead model), Cmd.none)
+    Tick time -> ( { model | body = updateBody model.attitude model.body, dead = isDead model } , Cmd.none)
     KeyCode k -> let
       dir = toDirection k
     in
       case dir of
         Nothing -> (model, Cmd.none)
         Just d -> if isPerpendicular d model.attitude
-          then (Model d (updateBody d model.body) (isDead model), Cmd.none)
+          then ( { model | attitude = d, body = updateBody d model.body, dead = isDead model } , Cmd.none)
           else (model, Cmd.none)
     Restart -> init
+    Resize h w -> ( { model | grid = Grid (w*100 // h) 100 }, Cmd.none )
 
 updatePosition : Direction -> Position -> Position
 updatePosition d pos =
@@ -92,15 +104,15 @@ isDead : Model -> Bool
 isDead model =
   case model.body of
     [] -> True
-    h :: t -> (isOnBorder h)  || isHeadOnTail h t
+    h :: t -> (isOnBorder model.grid h)  || isHeadOnTail h t
 
-isOnBorder : Position -> Bool
-isOnBorder pos =
+isOnBorder : Grid -> Position -> Bool
+isOnBorder grid pos =
   not <|
   (0 < pos.x)
-  && (pos.x < 100)
+  && (pos.x < grid.width)
   && (0 < pos.y)
-  && (pos.y < 100)
+  && (pos.y < grid.height)
 
 isHeadOnTail : Position -> List Position -> Bool
 isHeadOnTail h t = not <| List.isEmpty <| List.filter ((==) h) t
@@ -114,21 +126,20 @@ view model =
         button [ onClick Restart ] [ text "Restart" ]
       ]
     False ->
-      div []
+      div []  -- style "max-width: calc(100vw - 8px); max-height: calc(100vh - 8px);"
         [
-          div [style "float: left"]
+          svg [ version "1.1", baseProfile "full", width "100vw", height "calc(100vh - 4px)", viewBox (getViewBox model.grid), preserveAspectRatio "none"]
             [
-              svg [ version "1.1", baseProfile "full", width "800",  height "800", viewBox "0 0 100 100", style "border: black 4px solid"]
-                [
-                  g [ transform "matrix(1,0,0,-1,0,100)" ] (List.map block model.body)
-                ]
-            ],
-          div [style "float: right"] [text <| toString model]
+              g [ transform "matrix(1,0,0,-1,0,100)" ] (List.map block model.body)
+            ]
         ]
 
 block : Position -> Html.Html Msg
 block pos =
   rect [ x <| toString pos.x, y <| toString pos.y, width "1", height "1", fill "black"] []
+
+getViewBox : Grid -> String
+getViewBox {width, height} = "0 0 " ++ toString width ++ " 100"
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
@@ -136,8 +147,9 @@ subscriptions model =
     True -> Sub.none
     False ->
       Sub.batch [
-        Keyboard.downs KeyCode,
-        AnimationFrame.times Tick
+        Keyboard.downs KeyCode
+        , AnimationFrame.times Tick
+        , Window.resizes (\{height, width} -> Resize height width)
       ]
 
 
